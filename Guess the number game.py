ccd6916280605e83277df122a1b9d2dd86d3be15
qwertpi <qwertpi@users.noreply.github.com>
2020-08-25 18:36:25 +0100

    Fix mixed tab spaces indentation, correct inital score so scores match original game

--- Guess the number game.py
+++ Guess the number game.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(1, 100)
 
 turns = 1
-score = 55
+score = 50
 
 guess = int(input("Guess a number between 1 and 100:"))
 while guess != number:
@@ -13,14 +13,14 @@ while guess != number:
         guess = int(input("Guess a number betwwen 1 and 50:"))
         turns += 1
         if turns < 10:
-        	score -= 5
+            score -= 5
     elif guess > number:
         print("Too high, try again.")
         print("")
         guess = int(input("Guess a number betwwen 1 and 50:"))
         turns += 1
-		if turns < 10:
-			score -= 5
+        if turns < 10:
+            score -= 5
     if guess == number:
         print("Well Done! You guessed correctly.")
         print("You completed the game in", turns, "turns")
